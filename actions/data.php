9215b104fd9905d730b52261c1064e29cb8b0d5b
Oskar Risberg <kokarn@gmail.com>
2015-07-14 14:14:32 +0200

    Added search functionality. Closes #9

--- actions/data.php
+++ actions/data.php
@@ -1,34 +1,75 @@
 <?php
 include( '../includes/default.php' );
 
-$query = 'SELECT
-        posts.topic,
-        posts.topic_url,
-        posts.url,
-        posts.content,
-        posts.timestamp,
-        developers.nick,
-        developers.role,
-        accounts.identifier,
-        posts.source
-    FROM
-        posts,
-        developers,
-        accounts
-    WHERE
-        developers.id = posts.uid
-    AND
-        accounts.uid = posts.uid
-    AND
-        accounts.service = posts.source
-    ORDER BY
-        posts.timestamp
-    DESC
-    LIMIT
-        100';
-$PDO = $database->prepare( $query );
-$PDO->execute();
+if( isset( $_GET[ 'search' ] ) && strlen( $_GET[ 'search' ] ) > 0 ) :
+    $query = 'SELECT
+            posts.topic,
+            posts.topic_url,
+            posts.url,
+            posts.content,
+            posts.timestamp,
+            developers.nick,
+            developers.role,
+            accounts.identifier,
+            posts.source
+        FROM
+            posts,
+            developers,
+            accounts
+        WHERE
+            developers.id = posts.uid
+        AND
+            accounts.uid = posts.uid
+        AND
+            accounts.service = posts.source
+        AND
+            (
+                posts.topic LIKE :query
+                OR
+                posts.content LIKE :query
+                OR
+                developers.nick LIKE :query
+                OR
+                developers.role LIKE :query
+            )
+        ORDER BY
+            posts.timestamp
+        DESC
+        LIMIT
+            100';
 
+    $PDO = $database->prepare( $query );
+    $PDO->bindValue( ':query', '%' . $_GET[ 'search' ] . '%' );
+else :
+    $query = 'SELECT
+            posts.topic,
+            posts.topic_url,
+            posts.url,
+            posts.content,
+            posts.timestamp,
+            developers.nick,
+            developers.role,
+            accounts.identifier,
+            posts.source
+        FROM
+            posts,
+            developers,
+            accounts
+        WHERE
+            developers.id = posts.uid
+        AND
+            accounts.uid = posts.uid
+        AND
+            accounts.service = posts.source
+        ORDER BY
+            posts.timestamp
+        DESC
+        LIMIT
+            100';
+    $PDO = $database->prepare( $query );
+endif;
+
+$PDO->execute();
 $posts = $PDO->fetchAll();
 
 header( 'Content-Type: application/json' );
--- components/Postlist.jsx
+++ components/Postlist.jsx
@@ -3,15 +3,42 @@
 var React = require( 'react' );
 var Post = require( './Post.jsx' );
 var $ = require( 'jquery' );
+var debounce = require( 'debounce' );
+var queryString = require( 'query-string' );
 
 module.exports = React.createClass({
-    getInitialState: function() {
-        return {
-            data: []
-        };
+    updateDataTimeout : false,
+    setUpdateTimeout : function(){
+        this.updateDataTimeout = setTimeout( this.loadCommentsFromServer, this.props.pollInterval );
     },
-    loadCommentsFromServer: function() {
-        $.ajax({
+    getInitialState: function() {
+        var currentQuery = queryString.parse( location.search ),
+            defaultState =  {
+                data: [],
+                searchString: ''
+            };
+
+        if( typeof currentQuery.search !== 'undefined' ){
+            defaultState.searchString = currentQuery.search;
+        }
+
+        return defaultState;
+    },
+    handleSearch: function( event ){
+        event.persist();
+        this.debouncedSearch( event );
+    },
+    search: function( event ){
+        this.setState({
+            searchString: event.target.value
+        });
+
+        this.loadCommentsFromServer( event.target.value );
+    },
+    loadCommentsFromServer: function( searchString ) {
+        clearTimeout( this.updateDataTimeout );
+
+        var options = {
             url: this.props.url,
             dataType: 'json',
             cache: false,
@@ -19,15 +46,35 @@ module.exports = React.createClass({
                 this.setState({
                     data: data
                 });
+
+                this.setUpdateTimeout();
             }.bind( this ),
             error: function( xhr, status, err ) {
                 console.error( this.props.url, status, err.toString() );
+                this.setUpdateTimeout();
             }.bind( this )
-        });
+        };
+
+        if( typeof searchString !== 'undefined' ){
+            // This happends the first time
+            options.data = {
+                search: searchString
+            };
+        } else if( this.state.searchString.length > 0 ){
+            // This happends when we load posts again after some time
+            options.data = {
+                search: this.state.searchString
+            };
+        }
+
+        $.ajax( options );
+    },
+    componentWillMount: function() {
+        this.debouncedSearch = debounce( this.search, 250 );
     },
     componentDidMount: function() {
         this.loadCommentsFromServer();
-        setInterval( this.loadCommentsFromServer, this.props.pollInterval );
+        this.setUpdateTimeout();
     },
     render: function() {
         var postNodes = this.state.data.map( function( communityPost ) {
@@ -38,8 +85,21 @@ module.exports = React.createClass({
             );
         });
         return (
-            <div className="communityPostList">
-                {postNodes}
+            <div>
+                <form>
+                    <input
+                        type="text"
+                        role="search"
+                        name="search"
+                        className="form-control"
+                        placeholder="Search..."
+                        defaultValue={this.state.searchString}
+                        onChange={this.handleSearch}
+                    ></input>
+                </form>
+                <div className="communityPostList">
+                    {postNodes}
+                </div>
             </div>
         );
     }
