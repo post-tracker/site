e7356e492aa644d7afba82cbc09fcea94f1ba07b
Oskar Risberg <kokarn@gmail.com>
2015-07-15 16:08:16 +0200

    Add grunt-env for development

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,12 +28,19 @@ module.exports = function(grunt) {
                 src: [ 'scripts/app.built.js' ],
                 dest: 'scripts/app.min.js'
             }
+        },
+
+        env: {
+            dev : {
+                NODE_ENV : 'development'
+            }
         }
     });
 
     grunt.loadNpmTasks( 'grunt-browserify' );
     grunt.loadNpmTasks( 'grunt-contrib-watch' );
     grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+    grunt.loadNpmTasks( 'grunt-env' );
 
     grunt.registerTask( 'default', [
         'browserify'
