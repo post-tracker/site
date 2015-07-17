12b748ef9eb5488fc2e7dbef3229101eb8e29a72
Oskar Risberg <kokarn@gmail.com>
2015-07-17 10:01:37 +0200

    Add some more output when we deploy. Shouldn't be anything sensitive anyways

--- actions/deploy.php
+++ actions/deploy.php
@@ -3,8 +3,7 @@
 // Use in the "Webhooks" section of your GitHub repo.
 
 if( isset( $_SERVER[ 'HTTP_X_GITHUB_EVENT' ] ) && $_SERVER[ 'HTTP_X_GITHUB_EVENT' ] == 'push' ) :
-    shell_exec( 'cd /usr/share/nginx/html/ && git reset --hard HEAD && git pull' );
-    echo 'Site updated';
+    echo 'Site updated', shell_exec( 'cd /usr/share/nginx/html/ && git reset --hard HEAD && git pull' );
 else :
     echo 'Missing or unset parameters';
 endif;
