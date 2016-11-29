0bec6ac209b2cee49935ba2085dc288777bb81e3
kokarn <kokarn@gmail.com>
2016-11-29 17:26:33 +0100

    Add SSL settings. Closes #1

--- scripts/vagrant-bootstrap.sh
+++ scripts/vagrant-bootstrap.sh
@@ -17,8 +17,11 @@ sudo ln -s /vagrant/dist /usr/share/nginx/html
 sudo apt-get install -y php-fpm php-sqlite3 php-apcu php-xml
 
 # Setup /etc/nginx/sites-available/default
-sudo rm /etc/nginx/sites-available/default
-sudo ln -s /vagrant/config/nginx-vagrant /etc/nginx/sites-available/default
+rm /etc/nginx/sites-available/default
+cp /vagrant/config/server.conf /etc/nginx/server.conf
+cp /vagrant/config/ssl.conf /etc/nginx/ssl.conf
+cp /vagrant/config/vagrant.conf /etc/nginx/vagrant.conf
+ln -s /vagrant/config/nginx-vagrant /etc/nginx/sites-available/default
 
 sudo sed -i -e 's/display_errors = Off/display_errors = On/g' /etc/php/7.0/fpm/php.ini
 sudo sed -i -e 's/error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT/error_reporting = E_ALL/g' /etc/php/7.0/fpm/php.ini
