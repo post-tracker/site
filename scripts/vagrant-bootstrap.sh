dd14769bf2f19fbda6c4d7bc96d23f0634e95f31
kokarn <kokarn@gmail.com>
2016-11-29 18:02:07 +0100

    Remove extra sudo from vagrant bootstrap script

--- scripts/vagrant-bootstrap.sh
+++ scripts/vagrant-bootstrap.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # Something's fucky
-echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
+echo "nameserver 8.8.8.8" | tee /etc/resolv.conf > /dev/null
 
-sudo apt-get update
-sudo apt-get upgrade
+apt-get update
+apt-get upgrade
 
 # Installing node.js
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_6.x | -E bash -
+apt-get install -y nodejs
 
-sudo apt-get install -y nginx
-sudo rm -r /usr/share/nginx/html
-sudo ln -s /vagrant/dist /usr/share/nginx/html
+apt-get install -y nginx
+rm -r /usr/share/nginx/html
+ln -s /vagrant/dist /usr/share/nginx/html
 
-sudo apt-get install -y php-fpm php-sqlite3 php-apcu php-xml
+apt-get install -y php-fpm php-sqlite3 php-apcu php-xml
 
 # Setup /etc/nginx/sites-available/default
 rm /etc/nginx/sites-available/default
@@ -23,10 +23,10 @@ cp /vagrant/config/ssl.conf /etc/nginx/ssl.conf
 cp /vagrant/config/vagrant.conf /etc/nginx/vagrant.conf
 ln -s /vagrant/config/nginx-vagrant /etc/nginx/sites-available/default
 
-sudo sed -i -e 's/display_errors = Off/display_errors = On/g' /etc/php/7.0/fpm/php.ini
-sudo sed -i -e 's/error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT/error_reporting = E_ALL/g' /etc/php/7.0/fpm/php.ini
+sed -i -e 's/display_errors = Off/display_errors = On/g' /etc/php/7.0/fpm/php.ini
+sed -i -e 's/error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT/error_reporting = E_ALL/g' /etc/php/7.0/fpm/php.ini
 
-sudo apt-get install -y lynx
+apt-get install -y lynx
 
-sudo systemctl reload nginx
-sudo systemctl restart php7.0-fpm
+systemctl reload nginx
+systemctl restart php7.0-fpm
