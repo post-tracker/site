f5a1652461d8bff4ef5ade4aa4bb37324c5231d4
Oskar Risberg <kokarn@gmail.com>
2017-07-18 00:39:09 +0200

    Switch browserify for webpack
    Closes #19

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require( 'path' );
+
+module.exports = {
+    entry: './app/index.jsx',
+    module: {
+        rules: [
+            {
+                exclude: /(node_modules|bower_components)/,
+                test: /\.jsx?$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            'env',
+                            'react',
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+    output: {
+        filename: 'app.js',
+        path: path.resolve( __dirname, 'web/scripts' ),
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx',
+        ],
+    },
+};
