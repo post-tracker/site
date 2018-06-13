996b8aafd8c7d2f182f1d649a1c97c2c93f451d4
Oskar Risberg <oskar.risberg@stendahls.se>
2018-06-13 18:27:48 +0200

    Add Redux devtools stuff

--- app/store.js
+++ app/store.js
@@ -1,14 +1,13 @@
-import { createStore, applyMiddleware  } from 'redux';
+import { createStore, applyMiddleware, compose  } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
 import trackerApp from './reducers';
 
 export default function configureStore ( preloadedState ) {
-    return createStore(
-        trackerApp,
-        preloadedState,
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    return createStore( trackerApp, preloadedState, composeEnhancers(
         applyMiddleware(
             thunkMiddleware
         )
-    );
+    ) );
 }
