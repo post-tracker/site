2e4346d01dfa86928f0f234776f0fdb076559e59
Oskar Risberg <oskar.risberg@stendahls.se>
2019-09-29 13:25:37 +0200

    ... do stuff? Forgot what this is xD

--- scripts/modules/gamecss.js
+++ scripts/modules/gamecss.js
@@ -3,17 +3,23 @@ const fs = require( 'fs' );
 const https = require( 'https' );
 
 const sass = require( 'node-sass' );
+const postcss = require( 'postcss' );
+const cssnano = require( 'cssnano' );
 
 const LIGHT_SOURCE_FILE = path.join( __dirname, '..', '..', 'web-assets', 'theme-light.scss' );
 const DARK_SOURCE_FILE = path.join( __dirname, '..', '..', 'web-assets', 'theme-dark.scss' );
 
-module.exports = function( game, type ) {
+module.exports = function( game, type, targetFolder ) {
     const lightBaseResult = sass.renderSync( {
         file: LIGHT_SOURCE_FILE,
+        sourceMap: true,
+        outFile: path.join( path.dirname( LIGHT_SOURCE_FILE ), 'theme-light' ),
     } );
 
     const darkBaseResult = sass.renderSync( {
         file: DARK_SOURCE_FILE,
+        sourceMap: true,
+        outFile: path.join( path.dirname( DARK_SOURCE_FILE ), 'theme-dark' ),
     } );
 
     const baseStyles = {
@@ -24,7 +30,8 @@ module.exports = function( game, type ) {
 
     try {
         const gameSassResult = sass.renderSync( {
-            file: path.join( __dirname, '..', '..', 'games', identifier, `${ type }.scss` ),
+            file: path.join( __dirname, '..', '..', 'games', game, `theme-${ type }.scss` ),
+            sourceMap: true,
         } );
 
         if ( gameSassResult ) {
@@ -32,11 +39,23 @@ module.exports = function( game, type ) {
         }
     } catch( readError ) {
         // We don't care if we can't read it
+        if ( readError.status !== 3 ) {
+            console.log( readError );
+        }
     }
 
     if ( !gameStyles ) {
         gameStyles = baseStyles[ type ];
     }
+
+    postcss([cssnano])
+        .process(gameStyles, { from: 'src/app.css', to: 'dest/app.css' })
+        .then(result => {
+            fs.writeFile('dest/app.css', result.css, () => true)
+            if ( result.map ) {
+                fs.writeFile('dest/app.css.map', result.map, () => true)
+            }
+        })
 
     return gameStyles;
 };
