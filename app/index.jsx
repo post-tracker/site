3045a3f3f879cc8f4d30f579f1d99c9411f49891
Oskar Risberg <oskar.risberg@stendahls.se>
2020-03-18 09:08:16 +0100

    Move to proper style of setup

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import Ad from './components/Ad.jsx';
 import Root from './components/Root.jsx';
+
+render(
+    <Ad
+        dataAdSlot = { '9422842819' }
+    />,
+    document.getElementById( 'left-ad' )
+);
+
+render(
+    <Ad
+        dataAdSlot = { '7806508818' }
+    />,
+    document.getElementById( 'right-ad' )
+);
+
 
 render(
     <Root />,
