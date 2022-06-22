99c146d0d9a033a69eb0cee74210410d121853f8
Oskar Risberg <oskar.risberg@stendahls.se>
2022-06-22 18:41:02 +0200

    Add nicer dark-mode switch

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
 
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
 import PostListContainer from '../containers/PostListContainer.jsx';
 import FiltersContainer from '../containers/FiltersContainer.jsx';
 
-class App extends React.Component {
-    render () {
-        return (
-            <div>
-                <FiltersContainer />
-                <PostListContainer />
-            </div>
-        );
-    }
+import useStateWithLocalStorage from '../hooks/useStateWithLocalStorage.jsx';
+
+const App = () => {
+    const [theme, setTheme] = useStateWithLocalStorage('theme', 'theme-light');
+
+    const toggleDarkMode = (isDarkMode) => {
+        const newTheme = isDarkMode ? 'theme-light' : 'theme-dark';
+        setTheme(newTheme);
+        const event = new CustomEvent('switch-theme', {
+            detail: newTheme,
+        });
+
+        window.dispatchEvent(event);
+    };
+
+    return (
+        <div>
+            <DarkModeSwitch
+                checked={theme === 'theme-light' ? true : false }
+                onChange={toggleDarkMode}
+                moonColor={'black'}
+                sunColor={'white'}
+                style={{
+                    marginBottom: '2rem',
+                    position: 'absolute',
+                    top: '10px',
+                }}
+                size={24}
+            />
+            <FiltersContainer />
+            <PostListContainer />
+        </div>
+    );
 }
 
 export default App;
